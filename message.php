78310a1a48d271bab152eb21b00702dab00e4b37
Isabelle025 <isagrangeret@gmail.com>
2022-09-06 13:28:02 +0200

    responsive + formulaire

--- message.php
+++ message.php
@@ -1,7 +1,21 @@
 <?php
-    if (isset($_POST['msg'])) {
-        $retour = mail('isagrangeret@gmail.com', 'Envoi depuis la page Contact', $_POST['msg'], 'From: webmaster@monsite.fr' . "\r\n" . 'Reply-to: ' . $_POST['email']);           
-        if($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-    }
+    // if (isset($_POST['msg'])) {
+    //     $headers = 'From: '.$_POST['email']. "\r\n" .
+    // 'Reply-To: webmaster@example.com' . "\r\n" .
+    // 'X-Mailer: PHP/' . phpversion();
+    //     $retour = mail('i.grangeret@codeur.online', 'Envoi depuis la page Contact', $_POST['msg'], $headers);           
+    //     if($retour)
+    //     echo '<p>Votre message a bien été envoyé.</p>';
+    // }
+
+    // $to      = 'i.grangeret@codeur.online';
+    // $subject = 'le sujet';
+    // $message = 'Bonjour !';
+    // $headers = 'From: webmaster@example.com' . "\r\n" .
+    // 'Reply-To: webmaster@example.com' . "\r\n" .
+    // 'X-Mailer: PHP/' . phpversion();
+
+    // mail($to, $subject, $message, $headers);
+
+    
     ?>
