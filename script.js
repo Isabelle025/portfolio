868f6795908f590fc45498129d28c3bab3f31849
Isabelle025 <isagrangeret@gmail.com>
2023-02-22 10:58:32 +0100

    menu burger responsive

--- script.js
+++ script.js
@@ -327,3 +327,18 @@ form.addEventListener('input', debounce(function (e) {
             break;
     }
 }));
+
+// menu burger
+
+const openMobileMenuBtn = document.querySelector(".device-menu");
+const headerMenu = document.querySelector(".header-nav");
+
+openMobileMenuBtn.addEventListener("click", () => {
+  if(openMobileMenuBtn.classList.contains("open")) {
+    openMobileMenuBtn.classList.remove("open");
+    headerMenu.classList.remove("active");
+  } else {
+    headerMenu.classList.add("active");
+    openMobileMenuBtn.classList.add("open");
+  }
+})
